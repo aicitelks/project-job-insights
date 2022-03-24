b15ee0df03d4ff3898bbe6d8043cceeb20d2c47e
Letícia Castro <aicitelks@gmail.com>
2022-03-24 14:53:49 -0300

    Adicionado comentários explicativos para estudo futuro

--- src/jobs.py
+++ src/jobs.py
@@ -3,10 +3,15 @@ import csv
 
 
 @lru_cache
+# A FUNÇÃO SERÁ CHAMADA PELO TESTE QUE VAI PASSAR O PARÂMETRO: um arquivo .csv
 def read(path):
+    # ESSE ARQUIVO É ABERTO E É DADO O ALIAS csvfile
     with open(path) as csvfile:
+        # A FUNÇÃO DictReader TRANSFORMA O CONTEÚDO EM UM DICIONÁRIO
+        # JÁ COM CHAVE: VALOR, ONDE O CABEÇALHO SERÁ A CHAVE
         file_reader = csv.DictReader(csvfile)
         list_all_rows = []
         for row in file_reader:
+            # ADICIONO A MINHA VARIÁVEL CADA UMA DAS LINHAS DA "TABELA"
             list_all_rows.append(row)
     return list_all_rows
