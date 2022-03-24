c8fd40f792c604b72e5efdcd19e4bc3a896eb11e
Letícia Castro <aicitelks@gmail.com>
2022-03-24 14:47:13 -0300

    Requisito 1, função read, test: PASSED

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path) as csvfile:
+        file_reader = csv.DictReader(csvfile)
+        list_all_rows = []
+        for row in file_reader:
+            list_all_rows.append(row)
+    return list_all_rows
