851036ddb556e4eb6c56c5c958144117c0c8eb18
Letícia Castro <aicitelks@gmail.com>
2022-03-26 15:18:44 -0300

    Requisito 6, função filter_by_job_type test: PASSED

--- src/insights.py
+++ src/insights.py
@@ -10,21 +10,13 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    jobs_filtered = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    for job in jobs:
+        if job["job_type"] == job_type:
+            jobs_filtered.append(job)
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return jobs_filtered
 
 
 def get_unique_industries(path):
