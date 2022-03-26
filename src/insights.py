6f5a5d3ff3020a769e1b7a712ce8c538b5d1c03d
Letícia Castro <aicitelks@gmail.com>
2022-03-26 19:20:59 -0300

    Adicionado o comentário original, com as instruções da função matches_salary_range

--- src/insights.py
+++ src/insights.py
@@ -70,6 +70,26 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
+    """Checks if a given salary is in the salary range of a given job
+    Parameters
+    ----------
+    job : dict
+        The job with `min_salary` and `max_salary` keys
+    salary : int
+        The salary to check if matches with salary range of the job
+    Returns
+    -------
+    bool
+        True if the salary is in the salary range of the job, False otherwise
+    Raises
+    ------
+    ValueError
+        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
+        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
+        If `job["min_salary"]` is greather than `job["max_salary"]`
+        If `salary` isn't a valid integer
+    """
+
     # PARA CADA IF ESTÁ SENDO LEVANTADO O ERRO ValueError
     # CONFORME MOSTRA A DOCUMENTAÇÃO EM 8.4. Levantando exceções
     # (https://docs.python.org)
